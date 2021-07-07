79cb9dffa77cfc07387a4e9f12078004688bf759
matsoz <55518409+matsoz@users.noreply.github.com>
2021-07-07 19:53:25 -0300

    2. Upload route working and icon configuration

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/upload',(req,res) =>{
+
+  let form = new formidable.IncomingForm({
+    /* Searches for upload directory in the root folder 
+    *  and keep file extension avoiding corruption
+    */
+    uploadDir: './upload',
+    keepExtensions: true
+  });
+ 
+  form.parse(req,(err, fields, files)=>{
+        // Sends the files
+        res.json({
+          files
+        });
+  });
 });
 
 module.exports = router;
